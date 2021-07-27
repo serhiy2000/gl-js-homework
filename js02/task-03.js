742a6feaa7950b95bc587dde6a7916577f29a909
Serhii Perepeliuk <serhii.perepeliuk@globallogic.com>
2021-07-27 15:29:26 +0300

    Updates

---
+++ js02/task-03.js
@@ -0,0 +1,25 @@
+// Задание 3
+// Напиши фукцнию findLongestWord(string), которая принимает параметром произвольную строку (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
+
+const findLongestWord = function (string) {
+	string = string.split(' ');
+	let max = {
+		length: 0,
+	};
+	for (item in string) {
+		if (max.length < string[item].length) {
+			max.length = string[item].length;
+			max.word = string[item];
+		}
+	}
+	return max.word;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+console.log(findLongestWord('Google do a roll')); // 'Google'
+
+console.log(findLongestWord('May the force be with you')); // 'force'
